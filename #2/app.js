78e9130df2f6f40f1e3ee693e63a50042c4f2487
devetloper <simpson9713@naver.com>
2022-04-18 21:42:12 +0900

    Calculator

--- #2/app.js
+++ #2/app.js
@@ -1,6 +1,21 @@
-const a = 5;
-const b = 2;
+const calculator = {
+  ver : 1.0,
+  add : function(a,b){
+    console.log(a+ b)
+  },
+  minus : function(a,b){
+    console.log(a - b)
+  },
+  multiply : function(a,b){
+    console.log(a * b)
+  },
+  divide : function(a,b){
+    console.log(a / b)
+  }
+}
 
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
+calculator.add(2,7)
+calculator.minus(12,42)
+calculator.multiply(3222,47)
+calculator.divide(232,37)
+console.log(calculator.ver)
